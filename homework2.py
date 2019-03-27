0c1fde3ee04aa7a2a868e3ad5ce341a0f86e3bfe
root <root@zuse-debian.2n2.org>
2019-03-27 18:32:24 -0500

    Created basic program to compare two numbers

---
+++ homework2.py
@@ -0,0 +1,10 @@
+'''
+Author: Noelle East
+Purpose: The purpose of this program is
+         to compare two numbers
+'''
+first_num = int(input("Please enter one number :"))
+second_num = int(input("Please enter another number :"))
+
+if first_num > second_num:
+    print("num 1 is bigger than num 2")
